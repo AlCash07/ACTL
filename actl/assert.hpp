f1e1772cb1c2e6bb3ca0b7dc0935289fa098dba9
Oleksandr Bacherikov <Al.Cash07@gmail.com>
2017-10-14 00:19:07 +0300

    clang-format configuration file. File headers fix.

--- actl/assert.hpp
+++ actl/assert.hpp
@@ -1,5 +1,5 @@
-/***********************************************************************************************//**
- * Copyright (c) Oleksandr Bacherikov 2017.
+/***************************************************************************************************
+ * Copyright 2017 Oleksandr Bacherikov.
  *
  *             Distributed under the Boost Software License, Version 1.0.
  * (See accompanying file LICENSE.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
@@ -13,10 +13,10 @@
 #include <stdexcept>
 #include <string>
 
-#define ACTL_ASSERT(condition) \
-    if(!(condition)) { \
-        throw std::runtime_error(std::string() + #condition + \
-            " in line " + std::to_string(__LINE__) + " in " + __PRETTY_FUNCTION__); \
+#define ACTL_ASSERT(condition)                                                             \
+    if (!(condition)) {                                                                    \
+        throw std::runtime_error(std::string() + #condition + " in line " +                \
+                                 std::to_string(__LINE__) + " in " + __PRETTY_FUNCTION__); \
     }
 
 #else
