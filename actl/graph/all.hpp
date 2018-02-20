8d39c3ac508384f2aa12853d7b5d45ea6c4bc93b
Oleksandr Bacherikov <Al.Cash07@gmail.com>
2018-02-21 00:42:20 +0200

    Graph classes outline.

---
+++ actl/graph/all.hpp
@@ -0,0 +1,13 @@
+/***************************************************************************************************
+ * Copyright 2018 Oleksandr Bacherikov.
+ *
+ *             Distributed under the Boost Software License, Version 1.0.
+ * (See accompanying file LICENSE.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+ **************************************************************************************************/
+
+#pragma once
+
+#include <actl/graph/adjacency_list.hpp>
+#include <actl/graph/adjacency_matrix.hpp>
+#include <actl/graph/edge_list.hpp>
+#include <actl/graph/traversal/all.hpp>
