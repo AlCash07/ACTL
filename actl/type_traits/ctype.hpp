10c7e713fbdd5e3f18a7100c1fb73fb961220f80
Oleksandr Bacherikov <Al.Cash07@gmail.com>
2018-04-18 00:40:09 +0300

    ctype fix.

--- actl/type_traits/ctype.hpp
+++ actl/type_traits/ctype.hpp
@@ -9,14 +9,14 @@
 
 namespace ac {
 
-static inline constexpr bool is_space(char c) {
+inline constexpr bool is_space(char c) {
     return static_cast<unsigned char>(c - '\t') < 5 || c == ' ';
 }
 
-static inline constexpr bool is_digit(char c) { return static_cast<unsigned char>(c - '0') < 10; }
+inline constexpr bool is_digit(char c) { return static_cast<unsigned char>(c - '0') < 10; }
 
-static inline constexpr bool is_upper(char c) { return static_cast<unsigned char>(c - 'A') < 26; }
+inline constexpr bool is_upper(char c) { return static_cast<unsigned char>(c - 'A') < 26; }
 
-static inline constexpr bool is_lower(char c) { return static_cast<unsigned char>(c - 'a') < 26; }
+inline constexpr bool is_lower(char c) { return static_cast<unsigned char>(c - 'a') < 26; }
 
 }  // namespace ac
