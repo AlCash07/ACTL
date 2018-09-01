dba6eaa5d7ce44954b02eec218956e982f2001d9
Oleksandr Bacherikov <Al.Cash07@gmail.com>
2018-09-01 19:26:29 +0300

    Connected components were implemented.

--- actl/graph/connectivity/connected_components.hpp
+++ actl/graph/connectivity/connected_components.hpp
@@ -7,5 +7,36 @@
 
 #pragma once
 
+#include <actl/graph/default_property_map.hpp>
+#include <actl/graph/traversal/vertex_initializer.hpp>
+
 namespace ac {
+
+// Should be used only for undirected graphs.
+template <class Map>
+struct connected_component_recorder : property_map_wrapper_t<Map> {
+    using vertex = typename property_traits<Map>::key_type;
+
+    typename property_traits<Map>::value_type count = {};
+
+    void operator()(on_vertex_discover, vertex u) { put(*this, u, count); }
+    void operator()(on_search_finish) { ++count; }
+};
+
+template <class Map>
+inline connected_component_recorder<Map> make_connected_component_recorder(Map component) {
+    return {component};
 }
+
+template <class Map, class T>
+inline vertex_initializer<connected_component_recorder<Map>> make_connected_component_recorder(
+    Map&& component, T value) {
+    return {{std::forward<Map>(component)}, value};
+}
+
+template <class Graph>
+inline auto default_connected_component_recorder(const Graph& graph) {
+    return make_connected_component_recorder(default_vertex_property_map<int>(graph), -1);
+}
+
+}  // namespace ac
