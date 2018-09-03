6434407c3105725ad036a8029464e2bf36b1d468
Oleksandr Bacherikov <Al.Cash07@gmail.com>
2018-09-03 20:37:07 +0300

    Functions test.

--- actl/functions.hpp
+++ actl/functions.hpp
@@ -10,18 +10,13 @@
 namespace ac {
 
 template <class T>
-inline constexpr T& smin(T& x, const T& y) {
-    return y < x ? x = y : x;
-}
-
-template <class T>
-inline constexpr T& smax(T& x, const T& y) {
-    return x < y ? x = y : x;
-}
-
-template <class T>
 inline constexpr T abs(const T& x) {
     return x < T{0} ? -x : x;
+}
+
+template <class T>
+inline constexpr T hash_combine(T lhs, T rhs) {
+    return lhs ^ ((lhs << 6) + (lhs >> 2) + 0x9e3779b9 + rhs);
 }
 
 template <class T>
@@ -30,13 +25,18 @@ inline constexpr int sgn(const T& x, const T& y = T{0}) {
 }
 
 template <class T>
-inline constexpr T sqr(const T& x) {
-    return x * x;
+inline constexpr T& smax(T& x, const T& y) {
+    return x < y ? x = y : x;
 }
 
 template <class T>
-inline constexpr T hash_combine(T lhs, T rhs) {
-    return lhs ^ ((lhs << 6) + (lhs >> 2) + 0x9e3779b9 + rhs);
+inline constexpr T& smin(T& x, const T& y) {
+    return y < x ? x = y : x;
+}
+
+template <class T>
+inline constexpr T sqr(const T& x) {
+    return x * x;
 }
 
 }  // namespace ac
--- tests/functions.cpp
+++ tests/functions.cpp
@@ -7,7 +7,46 @@
 
 #include <actl/functions.hpp>
 #include <actl/test.hpp>
+#include <limits>
 
 using namespace ac;
 
-TEST("") {}
+TEST("abs") {
+    ASSERT_EQUAL(abs(0), 0);
+    constexpr auto int_max = std::numeric_limits<int>::max();
+    ASSERT_EQUAL(abs(int_max), int_max);
+    ASSERT_EQUAL(abs(-int_max), int_max);
+    constexpr auto uint_max = std::numeric_limits<unsigned int>::max();
+    ASSERT_EQUAL(abs(uint_max), uint_max);
+}
+
+TEST("sgn") {
+    ASSERT_EQUAL(sgn(std::numeric_limits<int>::max()), 1);
+    ASSERT_EQUAL(sgn(std::numeric_limits<int>::min()), -1);
+    ASSERT_EQUAL(sgn(std::numeric_limits<unsigned int>::max()), 1);
+    ASSERT_EQUAL(sgn(0), 0);
+    ASSERT_EQUAL(sgn(0, -1), 1);
+    ASSERT_EQUAL(sgn(0u, 1u), -1);
+}
+
+TEST("smax") {
+    int x = 4;
+    ASSERT_EQUAL(smax(x, 5), 5);
+    ASSERT_EQUAL(x, 5);
+    ASSERT_EQUAL(smax(x, 3), 5);
+    ASSERT_EQUAL(x, 5);
+}
+
+TEST("smin") {
+    int x = 4;
+    ASSERT_EQUAL(smin(x, 5), 4);
+    ASSERT_EQUAL(x, 4);
+    ASSERT_EQUAL(smin(x, 3), 3);
+    ASSERT_EQUAL(x, 3);
+}
+
+TEST("sqr") {
+    ASSERT_EQUAL(sqr(0), 0);
+    ASSERT_EQUAL(sqr(2), 4);
+    ASSERT_EQUAL(sqr(-2), 4);
+}
