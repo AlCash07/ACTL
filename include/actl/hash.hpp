215a35d497b8b0ed9d110655b1132dea109b994d
Oleksandr Bacherikov <Al.Cash07@gmail.com>
2018-09-06 23:47:20 +0300

    Year fix.

--- include/actl/hash.hpp
+++ include/actl/hash.hpp
@@ -1,5 +1,5 @@
 /***************************************************************************************************
- * Copyright 2017 Oleksandr Bacherikov.
+ * Copyright 2018 Oleksandr Bacherikov.
  *
  *             Distributed under the Boost Software License, Version 1.0.
  * (See accompanying file LICENSE.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
