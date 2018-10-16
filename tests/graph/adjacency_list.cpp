8b9397c37302cedfc066b4c4a4066fb49a99a409
Oleksandr Bacherikov <Al.Cash07@gmail.com>
2018-10-16 14:12:55 +0300

    adjacency_list test now compiles faster.

--- tests/graph/adjacency_list.cpp
+++ tests/graph/adjacency_list.cpp
@@ -31,7 +31,6 @@ template <class Dir, class OEC, class EC>
 void test_adjacency_list3() {
     test_adjacency_list<Dir, OEC, EC, std::vector<bundle>>();
     if constexpr (std::is_same_v<Dir, directed> || !std::is_same_v<value_type_t<EC>, one_vertex>) {
-        test_adjacency_list<Dir, OEC, EC, std::list<bundle>>();
         test_adjacency_list<Dir, OEC, EC, std::set<bundle>>();
         // TODO: fix the use of undeclared type as a template parameter and uncomment this line.
         // test_adjacency_list<Dir, OEC, EC, std::unordered_multiset<bundle>>();
@@ -42,16 +41,12 @@ template <class Dir, class OEC>
 void test_adjacency_list2() {
     test_adjacency_list3<Dir, OEC, none>();
     test_adjacency_list3<Dir, OEC, std::list<edge_property>>();
-    test_adjacency_list3<Dir, OEC, std::list<one_vertex>>();
-    test_adjacency_list3<Dir, OEC, std::list<two_vertices>>();
-    test_adjacency_list3<Dir, OEC, std::vector<edge_property>>();
     test_adjacency_list3<Dir, OEC, std::vector<one_vertex>>();
     test_adjacency_list3<Dir, OEC, std::vector<two_vertices>>();
 }
 
 template <class Dir>
 void test_adjacency_list1() {
-    test_adjacency_list2<Dir, std::list<bundle>>();
     test_adjacency_list2<Dir, std::set<bundle>>();
     test_adjacency_list2<Dir, std::unordered_multiset<bundle>>();
     test_adjacency_list2<Dir, std::vector<bundle>>();
