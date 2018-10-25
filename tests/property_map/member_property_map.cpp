f3107779632f8346a55703cf103f880db58356c4
Oleksandr Bacherikov <Al.Cash07@gmail.com>
2018-10-26 00:11:31 +0300

    member_property_map test.

--- tests/property_map/member_property_map.cpp
+++ tests/property_map/member_property_map.cpp
@@ -10,4 +10,24 @@
 
 using namespace ac;
 
-TEST("") {}
+struct A {
+    int a;
+};
+
+TEST("static") {
+    A x{0};
+    static_member_property_map<&A::a> mpm;
+    get(mpm, x) = 1;
+    ASSERT_EQUAL(1, x.a);
+    static_const_member_property_map<&A::a> cmpm;
+    ASSERT_EQUAL(1, get(cmpm, x));
+}
+
+TEST("non-static") {
+    A x{0};
+    auto mpm = make_member_property_map(&A::a);
+    get(mpm, x) = 2;
+    ASSERT_EQUAL(2, x.a);
+    auto cmpm = make_const_member_property_map(&A::a);
+    ASSERT_EQUAL(2, get(cmpm, x));
+}
