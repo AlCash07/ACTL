68cab88cf219db0c50478c3f1526e53a08c5a40e
AlCash07 <Al.Cash07@gmail.com>
2019-04-28 11:45:50 -0700

    Span improvement.

--- include/actl/util/span.hpp
+++ include/actl/util/span.hpp
@@ -7,6 +7,8 @@
 
 #pragma once
 
+#include <actl/assert.hpp>
+#include <actl/types.hpp>
 #include <iterator>
 #include <type_traits>
 
@@ -17,38 +19,44 @@ class span {
 public:
     using element_type = T;
     using value_type = std::remove_cv_t<T>;
-    using index_type = std::ptrdiff_t;
-    using difference_type = std::ptrdiff_t;
+    using index_type = index;
+    using difference_type = index;
     using pointer = T*;
     using reference = T&;
     using iterator = pointer;
 
     constexpr span() = default;
 
-    constexpr span(T* ptr, index_type count) : data_{ptr}, size_{count} {}
+    constexpr span(T* ptr, index count) : data_{ptr}, size_{count} { ACTL_ASSERT(count >= 0); }
 
     constexpr span(T* first, T* last) : span{first, last - first} {}
 
     template <class Range>
-    constexpr span(Range& r) : span{std::data(r), static_cast<index_type>(std::size(r))} {}
+    constexpr span(Range& r) : span{std::data(r), static_cast<index>(std::size(r))} {}
 
     constexpr T* begin() const { return data(); }
 
     constexpr T* end() const { return data() + size(); }
 
-    constexpr T& back() const { return data()[size() - 1]; }
+    constexpr T& back() const {
+        ACTL_ASSERT(data() && !empty());
+        return data()[size() - 1];
+    }
 
-    constexpr T& operator[](index_type i) const { return data()[i]; }
+    constexpr T& operator[](index i) const {
+        ACTL_ASSERT(data() && 0 <= i && i < size());
+        return data()[i];
+    }
 
     constexpr T* data() const { return data_; }
 
-    constexpr index_type size() const { return size_; }
+    constexpr index size() const { return size_; }
 
     constexpr bool empty() const { return size() == 0; }
 
 private:
     T* data_ = nullptr;
-    index_type size_ = 0;
+    index size_ = 0;
 };
 
 template <class Range>
