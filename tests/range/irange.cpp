db8e51848a10f0e230d3bd86a8b7eeee9d8188c7
AlCash07 <Al.Cash07@gmail.com>
2019-07-03 00:09:49 -0700

    irange test.

--- tests/range/irange.cpp
+++ tests/range/irange.cpp
@@ -5,7 +5,58 @@
  * (See accompanying file LICENSE.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
  **************************************************************************************************/
 
+#include <actl/range/algorithm.hpp>
 #include <actl/range/irange.hpp>
 #include <actl/test.hpp>
 
-TEST("") {}
+using VI = std::vector<int>;
+
+TEST("irange") {
+    VI v;
+    copy(irange(2, 0), std::back_inserter(v));
+    copy(irange(2, 2), std::back_inserter(v));
+    ASSERT_EQUAL(VI{}, v);
+    copy(irange(2, 5), std::back_inserter(v));
+    ASSERT_EQUAL(VI{2, 3, 4}, v);
+    v = {};
+    copy(irange(2), std::back_inserter(v));
+    ASSERT_EQUAL(VI{0, 1}, v);
+}
+
+TEST("drange") {
+    VI v;
+    copy(drange(2, 4), std::back_inserter(v));
+    ASSERT_EQUAL(VI{}, v);
+    copy(drange(2, 2), std::back_inserter(v));
+    ASSERT_EQUAL(VI{2}, v);
+    copy(drange(-1, -2), std::back_inserter(v));
+    ASSERT_EQUAL(VI{2, -1, -2}, v);
+    v = {};
+    copy(drange(2), std::back_inserter(v));
+    ASSERT_EQUAL(VI{2, 1, 0}, v);
+}
+
+TEST("irange with step > 0") {
+    VI v;
+    copy(irange(2, 0, 2), std::back_inserter(v));
+    copy(irange(2, 2, 2), std::back_inserter(v));
+    ASSERT_EQUAL(VI{}, v);
+    copy(irange(2, 6, 2), std::back_inserter(v));
+    ASSERT_EQUAL(VI{2, 4}, v);
+    v = {};
+    copy(irange(-5, -2, 2), std::back_inserter(v));
+    ASSERT_EQUAL(VI{-5, -3}, v);
+}
+
+TEST("irange with step < 0") {
+    VI v;
+    copy(irange(2, 4, -2), std::back_inserter(v));
+    ASSERT_EQUAL(VI{}, v);
+    copy(irange(2, 2, -2), std::back_inserter(v));
+    ASSERT_EQUAL(VI{2}, v);
+    copy(irange(5, 3, -2), std::back_inserter(v));
+    ASSERT_EQUAL(VI{2, 5, 3}, v);
+    v = {};
+    copy(irange(-2, -5, -2), std::back_inserter(v));
+    ASSERT_EQUAL(VI{-2, -4}, v);
+}
