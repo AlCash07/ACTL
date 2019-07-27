331fc1dccd3213045ef35cfcdce298367ed24e3d
AlCash07 <Al.Cash07@gmail.com>
2019-07-27 12:08:36 -0700

    Point to line distance test.

--- include/actl/geometry/algorithm/distance/point_line.hpp
+++ include/actl/geometry/algorithm/distance/point_line.hpp
@@ -9,30 +9,16 @@
 
 #include <actl/geometry/algorithm/area/point_line.hpp>
 #include <actl/geometry/algorithm/distance/point_point.hpp>
-#include <actl/geometry/line.hpp>
 
 namespace ac {
 
-template <class P = use_default, class F = use_default, class NormPolicy = use_default,
-          class AreaPolicy = standard_area_points<P, F>>
-struct distance_point_line : deduce_t<NormPolicy, standard_norm<P, F>> {
-    AreaPolicy area_policy;
-};
-
-template <class P, class F, class NP, class AP, index N, class T0, class T1, class K>
-inline auto distance(const distance_point_line<P, F, NP, AP>& policy, const point<T0, N>& p,
-                     const line<T1, N, K>& l) {
-    if (l.start_kind() != endpoint::free && dot(policy, p - l.start, l.vector) <= 0)
+template <class Policy, index N, class T0, class T1, class K>
+inline auto distance(Policy&& policy, const point<T0, N>& p, const line<T1, N, K>& l) {
+    if (l.start_kind() != endpoint::free && less(policy, dot(policy, p - l.start, l.vector), 0))
         return norm(policy, p - l.start);
-    if (l.end_kind() != endpoint::free && dot(policy, p - l.end(), l.vector) >= 0)
+    if (l.end_kind() != endpoint::free && less(policy, 0, dot(policy, p - l.end(), l.vector)))
         return norm(policy, p - l.end());
-    return static_cast<geometry::float_t<F, T0, T1>>(adl::abs(area(policy.area_policy, p, l))) /
-           norm(policy, l.vector);
-}
-
-template <index N, class T0, class T1, class K>
-inline auto distance(use_default, const point<T0, N>& p, const line<T1, N, K>& l) {
-    return distance(distance_point_line{}, p, l);
+    return ratio(policy, adl::abs(area(policy, p, l)), norm(policy, l.vector));
 }
 
 }  // namespace ac
--- tests/geometry/algorithm/distance/point_line.cpp
+++ tests/geometry/algorithm/distance/point_line.cpp
@@ -5,8 +5,21 @@
  * (See accompanying file LICENSE.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
  **************************************************************************************************/
 
-#include <actl/geometry/algorithm/distance/distance.hpp>
 #include <actl/geometry/algorithm/distance/point_line.hpp>
 #include <actl/test.hpp>
 
-TEST("") {}
+TEST("horizontal segment") {
+    segment<int> l{{2, 0}, {7, 0}};
+    ASSERT_EQUAL(0.0, distance(l, point{6, 0}));
+    ASSERT_EQUAL(2.0, distance(point{3, 2}, l));
+    ASSERT_EQUAL(2.0, distance(point{5, -2}, l));
+    ASSERT_EQUAL(5.0, distance(point{-1, 4}, l));
+    ASSERT_EQUAL(5.0, distance(point{11, -3}, l));
+}
+
+TEST("horizontal line") {
+    line<int> l{{2, 0}, {7, 0}};
+    ASSERT_EQUAL(0.0, distance(l, point{7, 0}));
+    ASSERT_EQUAL(4.0, distance(point{-1, 4}, l));
+    ASSERT_EQUAL(3.0, distance(point{11, -3}, l));
+}
