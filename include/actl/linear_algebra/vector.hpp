4a8eea91430c7b0819a497d7f11ebb523267243a
AlCash07 <Al.Cash07@gmail.com>
2020-01-18 11:56:35 -0800

    Placeholder for linear algebra.

---
+++ include/actl/linear_algebra/vector.hpp
@@ -0,0 +1,14 @@
+/***************************************************************************************************
+ * Copyright 2020 Oleksandr Bacherikov.
+ *
+ *             Distributed under the Boost Software License, Version 1.0.
+ * (See accompanying file LICENSE.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+ **************************************************************************************************/
+
+#pragma once
+
+#include <actl/functional/vector.hpp>
+
+namespace ac::op {
+
+}  // namespace ac::op
