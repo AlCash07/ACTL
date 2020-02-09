dfe3b45a35e8cadd506c64fe957ca4da0fe9593c
AlCash07 <Al.Cash07@gmail.com>
2020-02-08 16:50:37 -0800

    std::data extension for smart pointers.

--- tests/container/traits.cpp
+++ tests/container/traits.cpp
@@ -8,6 +8,7 @@
 #include <actl/container/traits.hpp>
 #include <actl/std/vector.hpp>
 #include <actl/test.hpp>
+#include <memory>
 
 TEST("array::category") {
     using C = int[2];
@@ -29,4 +30,11 @@ TEST("rebind_container") {
     ASSERT_TRUE(std::is_same_v<dummy_container, rebind_container_t<dummy_container, int>>);
     ASSERT_TRUE(std::is_same_v<dummy_container, rebind_container_t<std::vector<int>, none>>);
     ASSERT_TRUE(std::is_same_v<std::vector<int>, rebind_container_t<std::vector<none>, int>>);
+}
+
+TEST("is_smart_pointer") {
+    ASSERT_TRUE(is_smart_pointer<std::unique_ptr<float>>::value);
+    ASSERT_TRUE(is_smart_pointer<const std::unique_ptr<std::vector<int>>>::value);
+    ASSERT_TRUE(is_smart_pointer<std::shared_ptr<float>>::value);
+    ASSERT_FALSE(is_smart_pointer<std::vector<float>>::value);
 }
