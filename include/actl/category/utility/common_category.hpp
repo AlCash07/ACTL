2e4dc5af3c912fdb070ca42c861513f2e537a63c
AlCash07 <Al.Cash07@gmail.com>
2021-01-10 08:16:43 -0800

    Generalize common category.

--- include/actl/category/utility/common_category.hpp
+++ include/actl/category/utility/common_category.hpp
@@ -9,22 +9,50 @@
 
 namespace ac {
 
-template <class T, class U>
-constexpr auto common_category(T, U) {
-    constexpr index levelT = category_level_v<T>;
-    constexpr index levelU = category_level_v<U>;
-    if constexpr (levelT == levelU) {
-        if constexpr (std::is_same_v<T, U>)
-            return T{};
-        else
-            return common_category(typename T::base{}, typename U::base{});
-    } else if constexpr (levelT < levelU)
-        return common_category(T{}, typename U::base{});
-    else
-        return common_category(typename T::base{}, U{});
-}
+namespace detail {
+
+template <class T>
+struct category_wrap {
+    using type = T;
+};
+
+}  // namespace detail
 
 template <class... Ts>
-using common_category_t = decltype(common_category(std::declval<Ts>()...));
+using common_category_t = typename decltype((... && detail::category_wrap<Ts>()))::type;
+
+namespace detail {
+
+template <class T>
+constexpr auto base(category_wrap<T>) {
+    return category_wrap<typename T::base>{};
+}
+
+template <class T, class U>
+constexpr auto merge(category_wrap<T> lhs, category_wrap<U> rhs) {
+    if constexpr (std::is_same_v<T, U>)
+        return lhs;
+    else
+        return base(lhs) && base(rhs);
+}
+
+template <template <class...> class C, class... Ts, class... Us>
+constexpr auto merge(category_wrap<C<Ts...>>, category_wrap<C<Us...>>) {
+    return category_wrap<C<common_category_t<Ts, Us>...>>{};
+}
+
+template <class T, class U>
+constexpr auto operator&&(category_wrap<T> lhs, category_wrap<U> rhs) {
+    constexpr index llevel = category_level_v<T>;
+    constexpr index rlevel = category_level_v<U>;
+    if constexpr (llevel == rlevel)
+        return merge(lhs, rhs);
+    else if constexpr (llevel < rlevel)
+        return lhs && base(rhs);
+    else
+        return base(lhs) && rhs;
+}
+
+}  // namespace detail
 
 }  // namespace ac
