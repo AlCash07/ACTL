2c1b132d7edfd0c0d4df564a470e74e32cdd3ed7
Oleksandr Bacherikov <oleksandr.bacherikov@snapchat.com>
2021-09-27 10:46:09 -0700

    Tuple comparison

---
+++ include/actl/container/equal_sequence/tuple.hpp
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <actl/category/tuple.hpp>
+#include <actl/meta/type_traits.hpp>
+#include <tuple>
+
+namespace ac {
+
+template <class T, class U, size_t... Is>
+constexpr bool equal_tuple_impl(
+    const T& lhs, const U& rhs, std::index_sequence<Is...>) noexcept
+{
+    static_assert(noexcept((... && (std::get<Is>(lhs) == std::get<Is>(rhs)))));
+    return (... && (std::get<Is>(lhs) == std::get<Is>(rhs)));
+}
+
+template <class T, class U, enable_int_if<is_tuple_v<T> && is_tuple_v<U>> = 0>
+constexpr auto equal_sequence(const T& lhs, const U& rhs) noexcept
+{
+    if constexpr (std::tuple_size_v<T> != std::tuple_size_v<U>)
+        return std::false_type{};
+    else
+        return equal_tuple_impl(
+            lhs, rhs, std::make_index_sequence<std::tuple_size_v<T>>{});
+}
+
+} // namespace ac
