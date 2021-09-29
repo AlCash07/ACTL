3312084483decdba8c21bedd98861edcb2e19e56
Oleksandr Bacherikov <oleksandr.bacherikov@snapchat.com>
2021-09-28 18:41:42 -0700

    Complete tests for static arrays

--- tests/container/array/static_array.cpp
+++ tests/container/array/static_array.cpp
@@ -24,46 +24,72 @@ void test_static_array_type_traits()
 
 void test_static_array_constructors()
 {
-    using sa2 = ac::static_array<int, 3, 1>;
-    constexpr sa2 array;
-    static_assert(array == sa2{3, 1});
-    static_assert(array == sa2{3_c, 1_c});
-    CHECK_THROWS(sa2{3, 2});
-    static_assert(!std::is_constructible_v<sa2, int>);
-    static_assert(!std::is_constructible_v<sa2, int, int, int>);
-    static_assert(!std::is_constructible_v<sa2, int, void*>);
-    static_assert(!std::is_constructible_v<sa2, decltype(2_c), int>);
+    using sa31 = ac::static_array<int, 3, 1>;
+    constexpr sa31 array;
+    static_assert(array == sa31{3, 1});
+    static_assert(array == sa31{3_c, 1_c});
+    CHECK_THROWS(sa31{3, 2});
+    static_assert(!std::is_constructible_v<sa31, int>);
+    static_assert(!std::is_constructible_v<sa31, int, int, int>);
+    static_assert(!std::is_constructible_v<sa31, int, void*>);
+    static_assert(!std::is_constructible_v<sa31, decltype(2_c), int>);
     /* CTAD */
 #ifndef _MSC_VER // internal compiler error
     static_assert(ac::equal_same_type(array, ac::static_array{3_c, 1_c}));
 #endif
 }
 
+template <auto X>
+inline constexpr auto const_v = std::integral_constant<decltype(X), X>{};
+
+template <class T, T... Values, size_t... Is>
+void test_static_array_interface_impl(std::index_sequence<Is...>)
+{
+    using Array = ac::static_array<T, Values...>;
+    constexpr Array array;
+    /* size */
+    static_assert(noexcept(Array::size()));
+    static_assert(sizeof...(Values) == Array::size());
+    /* data */
+    static_assert(noexcept(Array::data()));
+    static_assert(((Values == Array::data()[Is]) && ...));
+    /* element access */
+    static_assert(ACTL_ASSERT_IS_NOEXCEPT() == noexcept(array[0]));
+    static_assert(((Values == array[Is]) && ...));
+    static_assert(((noexcept(array[const_v<Is>])) && ...));
+    static_assert(
+        (ac::equal_same_type(const_v<Values>, array[const_v<Is>]) && ...));
+    static_assert(((noexcept(array.template get<Is>())) && ...));
+    static_assert(
+        (ac::equal_same_type(array[const_v<Is>], array.template get<Is>()) &&
+         ...));
+    /* equality */
+    static_assert(noexcept(array == array));
+    static_assert(array == array);
+    static_assert(noexcept(array != array));
+    static_assert(!(array != array));
+}
+
+template <class T, T... Values>
+void test_static_array_interface()
+{
+    test_static_array_interface_impl<T, Values...>(
+        std::make_index_sequence<sizeof...(Values)>{});
+}
+
 } // namespace
 
 TEST_CASE("static_array")
 {
-    using empty_sa = ac::static_array<size_t>;
-    using sa2 = ac::static_array<int, 3, -1>;
-    test_static_array_type_traits<empty_sa>();
-    test_static_array_type_traits<sa2>();
+    test_static_array_type_traits<ac::static_array<size_t>>();
+    test_static_array_type_traits<ac::static_array<int, 3, -1>>();
     test_static_array_constructors();
-    constexpr sa2 array;
-    /* size */ {
-        static_assert(0 == empty_sa{}.size());
-        static_assert(2 == array.size());
-    }
-    /* element access */ {
-        static_assert(3 == array[0]);
-        static_assert(ac::equal_same_type(3_c, array[0_c]));
-        static_assert(-1 == array[1]);
-        static_assert(
-            ac::equal_same_type(std::integral_constant<int, -1>{}, array[1_c]));
-    }
+    test_static_array_interface<unsigned int>();
+    test_static_array_interface<size_t, 4>();
+    test_static_array_interface<int, 5, 3, 2>();
     SECTION("structured binding")
     {
-        static_assert(noexcept(array.get<1>()));
-        auto [x0, x1] = array; // can't be constexpr yet
+        auto [x0, x1] = ac::static_array<int, 3, -1>{}; // cant be constexpr yet
         CHECK(3 == x0);
         CHECK(-1 == x1);
     }
