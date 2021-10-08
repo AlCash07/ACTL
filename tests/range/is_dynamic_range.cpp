61817779185188cd5acc6ece26f8a6a904e839ea
Oleksandr Bacherikov <oleksandr.bacherikov@snapchat.com>
2021-10-08 16:34:37 -0700

    Fix is_dynamic_range for references

--- tests/range/is_dynamic_range.cpp
+++ tests/range/is_dynamic_range.cpp
@@ -2,6 +2,11 @@
 #include <vector>
 
 static_assert(ac::is_dynamic_range_v<std::vector<int>>);
+static_assert(ac::is_dynamic_range_v<const std::vector<int>&>);
+
 static_assert(ac::is_range_v<int[3]>);
 static_assert(!ac::is_dynamic_range_v<int[3]>);
+static_assert(ac::is_range_v<int (&)[3]>);
+static_assert(!ac::is_dynamic_range_v<int (&)[3]>);
+
 static_assert(!ac::is_dynamic_range_v<int>);
