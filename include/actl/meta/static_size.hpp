808e1d01b539657f181f1b56a010b0c5abd44f1f
Oleksandr Bacherikov <oleksandr.bacherikov@snapchat.com>
2021-10-07 20:17:13 -0700

    MSVC fixes

--- include/actl/meta/static_size.hpp
+++ include/actl/meta/static_size.hpp
@@ -11,8 +11,8 @@ struct static_size : size_constant<dynamic_size>
 {};
 
 template <class T>
-struct static_size<T, void_t<decltype(std::tuple_size<T>{})>>
-    : std::tuple_size<T>
+struct static_size<T, void_t<decltype(std::tuple_size<std::remove_cv_t<T>>{})>>
+    : std::tuple_size<std::remove_cv_t<T>>
 {};
 
 template <class T, size_t N>
