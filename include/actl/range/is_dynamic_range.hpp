a9e555310f37d052383956dac17f5fa6131b564b
Oleksandr Bacherikov <oleksandr.bacherikov@snapchat.com>
2021-10-08 16:34:37 -0700

    Fix is_dynamic_range for references

--- include/actl/range/is_dynamic_range.hpp
+++ include/actl/range/is_dynamic_range.hpp
@@ -7,6 +7,6 @@ namespace ac {
 
 template <class T>
 inline constexpr bool is_dynamic_range_v =
-    is_range_v<T>&& static_size_v<T> == dynamic_size;
+    is_range_v<T>&& static_size_v<std::remove_reference_t<T>> == dynamic_size;
 
 } // namespace ac
