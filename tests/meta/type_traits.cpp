fffb85b5d8f88b1b40a6c177a7f4bcc6dab749b7
Oleksandr Bacherikov <oleksandr.bacherikov@snapchat.com>
2021-11-02 12:49:32 -0700

    size_c

--- tests/meta/type_traits.cpp
+++ tests/meta/type_traits.cpp
@@ -12,6 +12,10 @@ static_assert(std::is_same_v<const int, add_const_if_t<true, int>>);
 
 static_assert(std::is_same_v<int, remove_cvref_t<volatile const int&>>);
 
+static_assert(std::is_same_v<
+              const std::integral_constant<size_t, 2>,
+              decltype(size_c<2>)>);
+
 static_assert(is_one_of_v<int, int>);
 static_assert(is_one_of_v<int, double, int, float>);
 static_assert(!is_one_of_v<int, const int, int&, int*>);
