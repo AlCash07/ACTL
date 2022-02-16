27232578727be27b816c102e2221177473b98f89
Oleksandr Bacherikov <oleksandr.bacherikov@snapchat.com>
2022-02-15 18:41:17 -0800

    Character classification: comments, noexcept and more functionality

--- include/actl/string/ctype.hpp
+++ include/actl/string/ctype.hpp
@@ -8,22 +8,44 @@
 
 namespace ac {
 
-inline constexpr bool is_digit(char c)
+/// Checks if @p c is a decimal digit.
+inline constexpr bool is_digit(char c) noexcept
 {
     return static_cast<unsigned char>(c - '0') < 10;
 }
 
-inline constexpr bool is_lower(char c)
+/// Checks if @p c is a lowercase letter.
+inline constexpr bool is_lower(char c) noexcept
 {
     return static_cast<unsigned char>(c - 'a') < 26;
 }
 
-inline constexpr bool is_upper(char c)
+/// Checks if @p c is an uppercase letter.
+inline constexpr bool is_upper(char c) noexcept
 {
     return static_cast<unsigned char>(c - 'A') < 26;
 }
 
-inline constexpr bool is_space(char c)
+/// Checks if @p c is an alphabetic character (a letter).
+inline constexpr bool is_alpha(char c) noexcept
+{
+    return is_upper(c & ~0x20);
+}
+
+/// Checks if @p c is an alphabetic character or a decimal digit.
+inline constexpr bool is_alphanumeric(char c) noexcept
+{
+    return is_alpha(c) || is_digit(c);
+}
+
+/// Checks if @p c is a whitespace character, that is one of:
+/// - space (0x20, ' ')
+/// - form feed (0x0c, '\f')
+/// - line feed (0x0a, '\n')
+/// - carriage return (0x0d, '\r')
+/// - horizontal tab (0x09, '\t')
+/// - vertical tab (0x0b, '\v')
+inline constexpr bool is_space(char c) noexcept
 {
     return static_cast<unsigned char>(c - '\t') < 5 || c == ' ';
 }
--- tests/string/ctype.cpp
+++ tests/string/ctype.cpp
@@ -5,6 +5,7 @@
 //   http://www.boost.org/LICENSE_1_0.txt).
 
 #include <actl/string/ctype.hpp>
+#include <cctype>
 #include "test.hpp"
 
 TEST_CASE("ctype correctness")
@@ -12,10 +13,12 @@ TEST_CASE("ctype correctness")
     for (char c = std::numeric_limits<char>::min();; ++c)
     {
         auto uc = static_cast<unsigned char>(c);
-        CHECK((isdigit(uc) != 0) == is_digit(c));
-        CHECK((islower(uc) != 0) == is_lower(c));
-        CHECK((isupper(uc) != 0) == is_upper(c));
-        CHECK((isspace(uc) != 0) == is_space(c));
+        CHECK((std::isdigit(uc) != 0) == ac::is_digit(c));
+        CHECK((std::islower(uc) != 0) == ac::is_lower(c));
+        CHECK((std::isupper(uc) != 0) == ac::is_upper(c));
+        CHECK((std::isalpha(uc) != 0) == ac::is_alpha(c));
+        CHECK((std::isalnum(uc) != 0) == ac::is_alphanumeric(c));
+        CHECK((std::isspace(uc) != 0) == ac::is_space(c));
         if (c == std::numeric_limits<char>::max())
             break;
     }
