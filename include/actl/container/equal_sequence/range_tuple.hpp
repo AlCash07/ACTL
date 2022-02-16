5841200cfeb7353a0293cb856455f8f5d3f01518
AlCash07 <Al.Cash07@gmail.com>
2022-02-16 15:57:16 -0800

    Add missing copyrights

--- include/actl/container/equal_sequence/range_tuple.hpp
+++ include/actl/container/equal_sequence/range_tuple.hpp
@@ -1,3 +1,9 @@
+// Copyright 2022 Oleksandr Bacherikov.
+//
+// Distributed under the Boost Software License, Version 1.0
+// (see accompanying file LICENSE.txt or copy at
+//   http://www.boost.org/LICENSE_1_0.txt).
+
 #pragma once
 
 #include <actl/container/equal_sequence/range.hpp>
