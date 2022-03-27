f9748af978d4d8cac181250b49b54e32e7417881
AlCash07 <Al.Cash07@gmail.com>
2022-03-27 15:02:12 -0700

    Documentation for deduce_noexcept

---
+++ doc/actl/functional/deduce_noexcept.rst
@@ -0,0 +1,30 @@
+deduce_noexcept
+================================
+
+C++ has automatic return type deduction since C++11.
+However, in C++20 there's still no support for automatic noexcept specification.
+The following utilities make is easier to write noexcept-correct code until we get `noexcept(auto)` in the language.
+
+One caveat worth noting when using these utilities is that operator `noexcept(T{})`
+checks not only that the constructor is non-throwing, but also the destructor.
+Unfortunately, `std::is_nothrow_constructible <https://en.cppreference.com/w/cpp/types/is_constructible>`_
+doesn't help either (see Notes).
+
+Outline
+-------
+
+#define :c:macro:`AC_DEDUCE_NOEXCEPT_AND_RETURN`
+
+#define :c:macro:`AC_DEDUCE_NOEXCEPT_DECLTYPE_AND_RETURN`
+
+#define :c:macro:`AC_DEDUCE_NOEXCEPT_AND_INITIALIZE`
+
+Detailed outline
+----------------
+
+.. doxygenfile:: actl/functional/deduce_noexcept.hpp
+
+Design
+------
+
+These utilities are inspired by `BOOST_HOF_RETURNS <https://www.boost.org/doc/libs/1_78_0/libs/hof/doc/html/include/boost/hof/returns.html>`_.
--- include/actl/functional/deduce_noexcept.hpp
+++ include/actl/functional/deduce_noexcept.hpp
@@ -8,6 +8,15 @@
 
 /// Macro that avoids code duplication by returning the given expression from
 /// the function and deducing the `noexcept` specification from it.
+/// For example, the following operator @code
+/// template <class T>
+/// bool operator > (T lhs, T rhs) noexcept(noexcept(rhs < lhs)) {
+///     return rhs < lhs;
+/// } @endcode
+/// can be rewritten as @code
+/// template <class T>
+/// bool operator > (T lhs, T rhs) AC_DEDUCE_NOEXCEPT_AND_RETURN(rhs < lhs)
+/// @endcode
 #define AC_DEDUCE_NOEXCEPT_AND_RETURN(...) \
     noexcept(noexcept(__VA_ARGS__))        \
     {                                      \
@@ -16,8 +25,19 @@
 
 /// Macro that avoids code duplication by returning the given expression from
 /// the function and deducing the `noexcept` specification and return type from
-/// it. This macro is different from AC_DEDUCE_NOEXCEPT_AND_RETURN, because it
-/// correctly disables the function when the expression is ill-formed (SFINAE)
+/// it. For example, the following operator @code
+/// template <class T>
+/// auto operator > (T lhs, T rhs) noexcept(noexcept(rhs < lhs))
+///     -> decltype(rhs < lhs) {
+///     return rhs < lhs;
+/// } @endcode
+/// can be rewritten as @code
+/// template <class T>
+/// auto operator > (T lhs, T rhs)
+///    AC_DEDUCE_NOEXCEPT_DECLTYPE_AND_RETURN(rhs < lhs)
+/// @endcode
+/// @note This macro is different from #AC_DEDUCE_NOEXCEPT_AND_RETURN, because
+/// it correctly disables a function when the expression is ill-formed (SFINAE)
 /// instead of defining a function that always produces a compilation error.
 #define AC_DEDUCE_NOEXCEPT_DECLTYPE_AND_RETURN(...)        \
     noexcept(noexcept(__VA_ARGS__))->decltype(__VA_ARGS__) \
@@ -27,7 +47,13 @@
 
 /// Macro that avoids code duplication by using the given expression as a member
 /// initializer list and deducing the `noexcept` specification from it.
-/// @note data members aren't supported, only base classes and delegated
+/// For example, the following constructor @code
+/// Derived(int x) noexcept(noexcept(Base{x})) : Base{x} {}
+/// @endcode
+/// can be rewritten as @code
+/// Derived(int x) AC_DEDUCE_NOEXCEPT_AND_INITIALIZE(Base{x}) {}
+/// @endcode
+/// @note Data members aren't supported, only base classes and delegated
 /// constructors.
 #define AC_DEDUCE_NOEXCEPT_AND_INITIALIZE(...) \
     noexcept(noexcept(__VA_ARGS__)) : __VA_ARGS__
