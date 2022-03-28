f4babddd2951ae554ab84210170ba11eb8eaf423
AlCash07 <Al.Cash07@gmail.com>
2022-03-27 22:22:47 -0700

    Refactor invoke_tuple

--- include/actl/functional/invoke_tuple.hpp
+++ include/actl/functional/invoke_tuple.hpp
@@ -6,40 +6,67 @@
 
 #pragma once
 
+#include <actl/functional/deduce_noexcept.hpp>
 #include <actl/meta/type_traits.hpp>
 #include <tuple>
 
 namespace ac {
 
-/// Finds the first function that accepts given arguments (it's required to
-/// exist) and returns its output.
-template <size_t I = 0, class Tuple, class... Args>
-constexpr decltype(auto) invoke_first_matching(
-    Tuple&& tuple_to_invoke, const Args&... args)
+namespace detail {
+
+template <size_t I, class Tuple, class... Args>
+constexpr size_t find_first_matching() noexcept
 {
-    using T = remove_cvref_t<Tuple>;
     static_assert(
-        I < std::tuple_size_v<T>, "no element with requested signature");
-    if constexpr (std::is_invocable_v<std::tuple_element_t<I, T>, Args...>)
-        // TODO: use std::invoke when it's constexpr.
-        return std::get<I>(tuple_to_invoke)(args...);
+        I < std::tuple_size_v<Tuple>, "no element with requested signature");
+    if constexpr (std::is_invocable_v<std::tuple_element_t<I, Tuple>, Args...>)
+        return I;
     else
-        return invoke_first_matching<I + 1>(tuple_to_invoke, args...);
+        return find_first_matching<I + 1, Tuple, Args...>();
 }
 
-template <size_t I = 0, class Tuple, class... Args>
-constexpr void invoke_all_matching(
-    [[maybe_unused]] Tuple&& tuple_to_invoke,
-    [[maybe_unused]] const Args&... args)
-{
-    using T = remove_cvref_t<Tuple>;
-    if constexpr (I < std::tuple_size_v<T>)
-    {
-        if constexpr (std::is_invocable_v<std::tuple_element_t<I, T>, Args...>)
-            // TODO: use std::invoke when it's constexpr.
-            std::get<I>(tuple_to_invoke)(args...);
-        invoke_all_matching<I + 1>(tuple_to_invoke, args...);
-    }
-}
+template <
+    class T,
+    class... Args,
+    enable_int_if<!std::is_invocable_v<T, Args...>> = 0>
+constexpr void invoke_if_can(T&&, Args&...) noexcept
+{}
+
+template <class T, class... Args>
+constexpr auto invoke_if_can(T&& callable, Args&... args)
+    // TODO: use std::invoke when it's constexpr.
+    AC_DEDUCE_NOEXCEPT_DECLTYPE_AND_RETURN(std::forward<T>(callable)(args...))
+
+template <size_t... Is, class Tuple, class... Args>
+constexpr void invoke_all_matching_impl(
+    std::index_sequence<Is...>, Tuple&& tuple_to_invoke, const Args&... args)
+    AC_DEDUCE_NOEXCEPT_AND_RETURN(
+        (...,
+         invoke_if_can(
+             std::get<Is>(std::forward<Tuple>(tuple_to_invoke)), args...)))
+
+} // namespace detail
+
+/// Invokes the first tuple element that is invocable with @p args
+/// (it's required to exist) and returns its result.
+template <
+    class Tuple,
+    class... Args,
+    size_t I = detail::find_first_matching<0, remove_cvref_t<Tuple>, Args...>()>
+constexpr decltype(auto) invoke_first_matching(
+    Tuple&& tuple_to_invoke, Args&&... args)
+    // TODO: use std::invoke when it's constexpr.
+    AC_DEDUCE_NOEXCEPT_AND_RETURN(std::get<I>(
+        std::forward<Tuple>(tuple_to_invoke))(std::forward<Args>(args)...))
+
+/// Invokes in order all the tuple elements that are invocable with @p args.
+/// @note @p args aren't forwarded, because they may be shared between multiple
+/// invocations.
+template <class Tuple, class... Args>
+constexpr void invoke_all_matching(Tuple&& tuple_to_invoke, Args&&... args)
+    AC_DEDUCE_NOEXCEPT_AND_RETURN(detail::invoke_all_matching_impl(
+        std::make_index_sequence<std::tuple_size_v<remove_cvref_t<Tuple>>>{},
+        std::forward<Tuple>(tuple_to_invoke),
+        args...))
 
 } // namespace ac
