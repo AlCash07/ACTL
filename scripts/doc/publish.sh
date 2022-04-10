360dfe0a461848b77a76d8e3b25e369aa2758513
AlCash07 <Al.Cash07@gmail.com>
2022-04-10 10:22:37 -0700

    Fix publish script

--- scripts/doc/publish.sh
+++ scripts/doc/publish.sh
@@ -2,9 +2,7 @@
 
 current_branch=$(eval "git rev-parse --abbrev-ref HEAD")
 git checkout doc &&
-cp -r build/doc/sphinx/_static . &&
-cp -r build/doc/sphinx/actl . &&
-cp build/doc/sphinx/* .
+cp -r build/doc/sphinx/* . &&
 git add .
 git commit -m "Documentation update"
 git push
