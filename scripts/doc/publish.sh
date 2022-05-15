90404c6e35432e5716917e5f7a05d6139f60af31
AlCash07 <Al.Cash07@gmail.com>
2022-05-15 16:21:06 -0700

    Update publishing script

--- scripts/doc/publish.sh
+++ scripts/doc/publish.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
 current_branch=$(eval "git rev-parse --abbrev-ref HEAD")
+./scripts/git/start_tracking_vscode.sh &&
 git checkout doc &&
+rm -r _static actl contributing &&
 cp -r build/doc/sphinx/* . &&
 git add .
 git commit -m "Documentation update"
 git push
 git checkout $current_branch
+./scripts/git/stop_tracking_vscode.sh
