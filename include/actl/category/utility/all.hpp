e78c37994b73fec6201045386c960da457f2201f
AlCash07 <Al.Cash07@gmail.com>
2022-06-10 15:59:28 -0700

    Remove major category

--- include/actl/category/utility/all.hpp
+++ include/actl/category/utility/all.hpp
@@ -9,5 +9,4 @@
 #include <actl/category/utility/category_level.hpp>
 #include <actl/category/utility/common_category.hpp>
 #include <actl/category/utility/is_subcategory_of.hpp>
-#include <actl/category/utility/major_category.hpp>
 #include <actl/category/utility/nesting_depth.hpp>
