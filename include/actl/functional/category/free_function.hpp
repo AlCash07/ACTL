c14ab6f30ad52324706cf11af01822dcd9521f12
AlCash07 <Al.Cash07@gmail.com>
2024-08-04 05:24:44 -0700

    Join implementation

--- include/actl/functional/category/free_function.hpp
+++ include/actl/functional/category/free_function.hpp
@@ -6,12 +6,40 @@
 
 #pragma once
 
-#include <actl/functional/category/detail/is_free_function.hpp>
+#include <actl/preprocessor/AC_UNPARENTHESIZED.hpp>
+#include <type_traits>
 
 namespace ac {
+
+namespace detail {
+template<class T>
+struct is_free_function : std::false_type {};
+} // namespace detail
 
 /// Concept of a free function.
 template<class T>
 concept FreeFunction = detail::is_free_function<T>::value;
 
+namespace detail {
+
+#define AC_FF_PARAMETERS()          \
+    AC_FF_NOEXCEPT((Parameters...)) \
+    AC_FF_NOEXCEPT((Parameters..., ...))
+
+#define AC_FF_NOEXCEPT(PARAMETERS) \
+    AC_FF_FULL(PARAMETERS, )       \
+    AC_FF_FULL(PARAMETERS, noexcept)
+
+#define AC_FF_FULL(PARAMETERS, NOEXCEPT)                                    \
+    template<class Return, class... Parameters>                             \
+    struct is_free_function<Return(AC_UNPARENTHESIZED PARAMETERS) NOEXCEPT> \
+        : std::true_type {};
+
+AC_FF_PARAMETERS()
+
+#undef AC_FF_PARAMETERS
+#undef AC_FF_NOEXCEPT
+#undef AC_FF_FULL
+
+} // namespace detail
 } // namespace ac
