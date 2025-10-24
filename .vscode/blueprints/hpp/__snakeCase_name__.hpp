092753d64817292c49d9b2ea539a9ee7ac5d2d6a
AlCash07 <Al.Cash07@gmail.com>
2025-10-24 09:55:13 -0700

    2025

--- .vscode/blueprints/hpp/__snakeCase_name__.hpp
+++ .vscode/blueprints/hpp/__snakeCase_name__.hpp
@@ -1,4 +1,4 @@
-// Copyright 2024 Oleksandr Bacherikov.
+// Copyright 2025 Oleksandr Bacherikov.
 //
 // Distributed under the Boost Software License, Version 1.0.
 // (See accompanying file LICENSE.txt or copy at
